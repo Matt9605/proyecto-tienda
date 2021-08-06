55f0e0d6a4fe5e11f46880c88998c050b7caf73f
camilosolarte12 <camilosolartebenavides@gmail.com>
2021-08-05 20:08:31 -0500

    creacion de migraciones

---
+++ database/migrations/2021_08_06_003915_create_ventas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVentasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ventas', function (Blueprint $table) {
+            $table->id();
+            $table->string('numero_factura');
+            $table->string('fecha');
+            $table->string('valor_venta');
+            $table->integer('vendendor_id');
+            $table->integer('tienda_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ventas');
+    }
+}
