3944790beb1d0e72eb823f8b3cbffb4f5c3944b6
alexagm <alegeo1704@gmail.com>
2021-08-05 20:00:45 -0500

    creacion de formulario

---
+++ resources/views/tienda/crear.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                <form>
+  <div class="form-group">
+    <label for="nombre">Nombre</label>
+    <input type="text" class="form-control" id="nombre" name="nombre">
+    </div>
+  <div class="form-group">
+    <label for="direccion">Direccion</label>
+    <input type="text" class="form-control" id="direccion" name="direccion">
+  </div>
+ <div class="form-group">
+    <label for="direccion">Teléfono</label>
+    <input type="text" class="form-control" id="telefono" name="telefono">
+  </div>
+  <button type="submit" class="btn btn-primary">Guardar</button>
+</form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
